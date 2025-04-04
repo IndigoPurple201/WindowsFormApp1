21910cea20e54552729490353c88cacaaf7d2f34
IndigoPurple201 <betogamboa201@hotmail.com>
2025-04-04 13:30:03 -0600

    Segundo Commit 04/04/25

--- Tipos.Designer.cs
+++ Tipos.Designer.cs
@@ -28,19 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tipos));
+            btnCerrar = new PictureBox();
+            txtFolio = new TextBox();
+            label1 = new Label();
+            txtDescripcion = new TextBox();
+            label2 = new Label();
+            boxRefaccion = new ComboBox();
+            label3 = new Label();
+            btnNuevo = new Button();
+            btnAceptar = new Button();
+            btnCancelar = new Button();
+            label4 = new Label();
+            label5 = new Label();
+            dgvTipos = new DataGridView();
+            btnActualizar = new Button();
+            btnCerrar2 = new Button();
+            btnEliminar = new Button();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvTipos).BeginInit();
             SuspendLayout();
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Cursor = Cursors.Hand;
+            btnCerrar.Image = (Image)resources.GetObject("btnCerrar.Image");
+            btnCerrar.Location = new Point(552, 12);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(25, 27);
+            btnCerrar.SizeMode = PictureBoxSizeMode.StretchImage;
+            btnCerrar.TabIndex = 36;
+            btnCerrar.TabStop = false;
+            // 
+            // txtFolio
+            // 
+            txtFolio.BorderStyle = BorderStyle.FixedSingle;
+            txtFolio.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtFolio.Location = new Point(140, 54);
+            txtFolio.Name = "txtFolio";
+            txtFolio.Size = new Size(245, 23);
+            txtFolio.TabIndex = 44;
+            txtFolio.KeyPress += txtFolio_KeyPress;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(31, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(106, 30);
+            label1.TabIndex = 43;
+            label1.Text = "NUMERO";
+            // 
+            // txtDescripcion
+            // 
+            txtDescripcion.BorderStyle = BorderStyle.FixedSingle;
+            txtDescripcion.Font = new Font("Segoe UI", 9F);
+            txtDescripcion.Location = new Point(140, 83);
+            txtDescripcion.Name = "txtDescripcion";
+            txtDescripcion.Size = new Size(245, 23);
+            txtDescripcion.TabIndex = 46;
+            txtDescripcion.TextChanged += txtDescripcion_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(31, 85);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 21);
+            label2.TabIndex = 45;
+            label2.Text = "DESCRIPCION";
+            // 
+            // boxRefaccion
+            // 
+            boxRefaccion.DropDownStyle = ComboBoxStyle.DropDownList;
+            boxRefaccion.FlatStyle = FlatStyle.Popup;
+            boxRefaccion.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            boxRefaccion.FormattingEnabled = true;
+            boxRefaccion.Location = new Point(140, 112);
+            boxRefaccion.Name = "boxRefaccion";
+            boxRefaccion.Size = new Size(245, 23);
+            boxRefaccion.TabIndex = 47;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(44, 114);
+            label3.Name = "label3";
+            label3.Size = new Size(93, 21);
+            label3.TabIndex = 48;
+            label3.Text = "REFACCION";
+            // 
+            // btnNuevo
+            // 
+            btnNuevo.Cursor = Cursors.Hand;
+            btnNuevo.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNuevo.Location = new Point(138, 153);
+            btnNuevo.Name = "btnNuevo";
+            btnNuevo.Size = new Size(89, 23);
+            btnNuevo.TabIndex = 49;
+            btnNuevo.Text = "NUEVO";
+            btnNuevo.UseVisualStyleBackColor = true;
+            btnNuevo.Click += btnNuevo_Click;
+            // 
+            // btnAceptar
+            // 
+            btnAceptar.Cursor = Cursors.Hand;
+            btnAceptar.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAceptar.Location = new Point(233, 153);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.Size = new Size(89, 23);
+            btnAceptar.TabIndex = 50;
+            btnAceptar.Text = "ACEPTAR";
+            btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Cursor = Cursors.Hand;
+            btnCancelar.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCancelar.Location = new Point(328, 153);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(89, 23);
+            btnCancelar.TabIndex = 51;
+            btnCancelar.Text = "CANCELAR";
+            btnCancelar.UseVisualStyleBackColor = true;
+            btnCancelar.Click += btnCancelar_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(31, 200);
+            label4.Name = "label4";
+            label4.Size = new Size(166, 21);
+            label4.TabIndex = 52;
+            label4.Text = "TIPOS REGISTRADOS";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(31, 221);
+            label5.Name = "label5";
+            label5.Size = new Size(354, 13);
+            label5.TabIndex = 53;
+            label5.Text = "SELECCIONE UNO O MAS REGISTROS PARA ACTUALIZAR O ELIMINAR";
+            // 
+            // dgvTipos
+            // 
+            dgvTipos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvTipos.Location = new Point(31, 247);
+            dgvTipos.Name = "dgvTipos";
+            dgvTipos.Size = new Size(528, 150);
+            dgvTipos.TabIndex = 54;
+            // 
+            // btnActualizar
+            // 
+            btnActualizar.Cursor = Cursors.Hand;
+            btnActualizar.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnActualizar.Location = new Point(280, 403);
+            btnActualizar.Name = "btnActualizar";
+            btnActualizar.Size = new Size(89, 23);
+            btnActualizar.TabIndex = 57;
+            btnActualizar.Text = "ACTUALIZAR";
+            btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // btnCerrar2
+            // 
+            btnCerrar2.Cursor = Cursors.Hand;
+            btnCerrar2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCerrar2.Location = new Point(470, 403);
+            btnCerrar2.Name = "btnCerrar2";
+            btnCerrar2.Size = new Size(89, 23);
+            btnCerrar2.TabIndex = 56;
+            btnCerrar2.Text = "CERRAR";
+            btnCerrar2.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Cursor = Cursors.Hand;
+            btnEliminar.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnEliminar.Location = new Point(375, 403);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(89, 23);
+            btnEliminar.TabIndex = 55;
+            btnEliminar.Text = "ELIMINAR";
+            btnEliminar.UseVisualStyleBackColor = true;
             // 
             // Tipos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(589, 417);
+            ClientSize = new Size(589, 453);
+            Controls.Add(btnActualizar);
+            Controls.Add(btnCerrar2);
+            Controls.Add(btnEliminar);
+            Controls.Add(dgvTipos);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(btnNuevo);
+            Controls.Add(btnAceptar);
+            Controls.Add(btnCancelar);
+            Controls.Add(label3);
+            Controls.Add(boxRefaccion);
+            Controls.Add(txtDescripcion);
+            Controls.Add(label2);
+            Controls.Add(txtFolio);
+            Controls.Add(label1);
+            Controls.Add(btnCerrar);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Tipos";
             Text = "Tipos";
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvTipos).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox btnCerrar;
+        private TextBox txtFolio;
+        private Label label1;
+        private TextBox txtDescripcion;
+        private Label label2;
+        private ComboBox boxRefaccion;
+        private Label label3;
+        private Button btnNuevo;
+        private Button btnAceptar;
+        private Button btnCancelar;
+        private Label label4;
+        private Label label5;
+        private DataGridView dgvTipos;
+        private Button btnActualizar;
+        private Button btnCerrar2;
+        private Button btnEliminar;
     }
 }
--- Tipos.cs
+++ Tipos.cs
@@ -1,20 +1,312 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinFormsApp1
 {
-    public partial class Tipos: Form
+    public partial class Tipos : Form
     {
+        public event Action TipoAgregado;
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        private System.Windows.Forms.Timer parpadeoTimer;
+        private int parpadeoContador = 0;
+        private Color bordeOriginal = Color.Black;
+        private Color panelOriginal;
+        private bool parpadeoActivo = false;
+
+        [DllImport("user32.dll")]
+        private static extern void MessageBeep(uint uType);
+        private const uint MB_ICONERROR = 0x10; // Sonido de error del sistema
+
+        private const int WM_SYSCOMMAND = 0x112;
+        private const int SC_MOVE = 0xF012;
+        private Control controlActivo = null;
+        private Point mouseDownLocation;
+        private ConexionSQL conexionSQL = new ConexionSQL();
         public Tipos()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Padding = new Padding(3);
+
+            this.MouseDown += new MouseEventHandler(Tipos_MouseDown);
+            //this.MouseMove += new MouseEventHandler(Marca_MouseMove)
+        }
+
+        private void Tipos_Load(object sender, EventArgs e)
+        {
+            BloquearControles(true);
+            conexionSQL.ProbarConexion();
+
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is TextBox || ctrl is ComboBox)
+                {
+                    ctrl.Enter += ControlSeleccionado;
+                }
+            }
+            this.Click += QuitarFoco;
+            ConfigurarDataGridView();
+            CargarDatosDGV();
+            ObtenerSiguienteNumero();
+            txtFolio.Enabled = false;
+        }
+
+        private void Tipos_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_SYSCOMMAND, SC_MOVE | 0x0001, 0);
+            }
+        }
+
+        private void ControlSeleccionado(object sender, EventArgs e)
+        {
+            if (sender is TextBox || sender is ComboBox)
+            {
+                controlActivo = sender as Control;
+            }
+        }
+
+        private void CargarDatosDGV()
+        {
+            try
+            {
+                string query = "SELECT tipos.id_tipo AS Numero, tipos.descripcion AS Descripcion, tipos.refaccion FROM tipos;";
+                using (SqlConnection connection = conexionSQL.ObtenerConexion())
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        dgvTipos.Rows.Clear();
+                        while (reader.Read())
+                        {
+                            int index = dgvTipos.Rows.Add();
+                            dgvTipos.Rows[index].Cells["Numero"].Value = reader["Numero"].ToString();
+                            dgvTipos.Rows[index].Cells["Descripcion"].Value = reader["Descripcion"].ToString();
+                            dgvTipos.Rows[index].Cells["Refaccion"].Value = reader["refaccion"].ToString();
+                        }
+                    }
+                }
+                dgvTipos.ClearSelection();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+        }
+
+        private void ConfigurarDataGridView()
+        {
+            dgvTipos.BackgroundColor = Color.White;
+            dgvTipos.BorderStyle = BorderStyle.None;
+
+
+            dgvTipos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+
+            dgvTipos.RowsDefaultCellStyle.BackColor = Color.LightGray;
+            dgvTipos.AlternatingRowsDefaultCellStyle.BackColor = Color.White;
+
+
+            dgvTipos.DefaultCellStyle.SelectionBackColor = Color.LightSkyBlue;
+            dgvTipos.DefaultCellStyle.SelectionForeColor = Color.White;
+
+
+            dgvTipos.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            if (dgvTipos.Columns.Count == 0)
+            {
+                dgvTipos.Columns.Add("Numero", "Número");
+                dgvTipos.Columns.Add("Descripcion", "Descripción");
+            }
+
+            dgvTipos.Columns["Descripcion"].ReadOnly = false;
+            dgvTipos.Columns["Numero"].ReadOnly = true;
+        }
+
+        private void txtFolio_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+            // Permitir solo números y la tecla de retroceso
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; // Bloquear entrada no numérica
+            }
+            // Evitar que se ingresen más de 4 dígitos
+            if (!char.IsControl(e.KeyChar) && txt.Text.Length >= 4)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtDescripcion_TextChanged(object sender, EventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+            // Convertir todo el texto a mayúsculas
+            txt.Text = txt.Text.ToUpper();
+            // Mover el cursor al final para evitar que vuelva atrás
+            txt.SelectionStart = txt.Text.Length;
+        }
+
+        private void LimpiarControles()
+        {
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is TextBox textBox)
+                {
+                    if (textBox.Name != "txtFolio")
+                    {
+                        textBox.Clear();
+                    }
+                }
+            }
+        }
+
+        private void BloquearControles(bool bloquear)
+        {
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is TextBox && ctrl != txtFolio)
+                {
+                    ctrl.Enabled = !bloquear; // Deshabilita o habilita los controles
+                }
+            }
+            btnNuevo.Enabled = bloquear;    // "Nuevo" solo está habilitado cuando los demás están bloqueados
+            btnAceptar.Enabled = !bloquear; // "Aceptar" solo se habilita cuando los controles están activos
+            btnCancelar.Enabled = !bloquear; // "Cancelar" solo se habilita cuando los controles están activos
+            boxRefaccion.Enabled = !bloquear; // "Refacción" solo se habilita cuando los controles están activos
+        }
+
+        private void ObtenerSiguienteNumero()
+        {
+            try
+            {
+                using (SqlConnection connection = conexionSQL.ObtenerConexion())
+                {
+                    connection.Open();
+                    string query = "SELECT ISNULL(MAX(id_tipo), 0) + 1 AS SiguienteNumero FROM tipos;";
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        object result = cmd.ExecuteScalar();
+                        txtFolio.Text = result.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            BloquearControles(false);
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimpiarControles();
+            BloquearControles(true);
+        }
+
+        private void QuitarFoco(object sender, EventArgs e)
+        {
+            if (controlActivo != null && !controlActivo.Bounds.Contains(this.PointToClient(Cursor.Position)))
+            {
+                this.ActiveControl = null;
+                controlActivo = null;
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_NCACTIVATE = 0x86;
+
+            if (m.Msg == WM_NCACTIVATE && m.WParam.ToInt32() == 0 && !parpadeoActivo)
+            {
+                IniciarParpadeo();
+                MessageBeep(MB_ICONERROR);
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void IniciarParpadeo()
+        {
+            parpadeoActivo = true; // Indica que el parpadeo está en proceso
+            parpadeoContador = 0;
+
+            if (parpadeoTimer == null)
+            {
+                parpadeoTimer = new System.Windows.Forms.Timer();
+                parpadeoTimer.Interval = 150; // 150 ms
+                parpadeoTimer.Tick += (sender, e) =>
+                {
+                    if (parpadeoContador >= 6) // 3 ciclos de parpadeo
+                    {
+                        parpadeoTimer.Stop();
+                        bordeOriginal = Color.Black; // Restaurar borde
+                        this.Invalidate(); // Redibujar
+                        parpadeoActivo = false; // Permitir que se active de nuevo si es necesario
+                    }
+                    else
+                    {
+                        // Alternar entre blanco y negro
+                        bordeOriginal = (bordeOriginal == Color.Black) ? Color.White : Color.Black;
+                        this.Invalidate();
+                        parpadeoContador++;
+                    }
+                };
+            }
+            parpadeoTimer.Start();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            using (Pen pen = new Pen(bordeOriginal, 3))
+            {
+                e.Graphics.DrawRectangle(pen, new Rectangle(0, 0, this.Width - 1, this.Height - 1));
+            }
+        }
+
+        private bool validarCampos()
+        {
+            bool esValido = true;
+            string mensajeError = "Faltan los siguientes campos:\n";
+            if (string.IsNullOrWhiteSpace(txtFolio.Text))
+            {
+                mensajeError += "- Especifique un numero valido.\n";
+                esValido = false;
+            }
+            if (string.IsNullOrEmpty(txtDescripcion.Text))
+            {
+                mensajeError += "- Especifique una descripcion valida.\n";
+                esValido = false;
+            }
+            if (boxRefaccion.SelectedIndex == 1 || string.IsNullOrWhiteSpace(boxRefaccion.Text))
+            {
+                mensajeError += "- Especifique una refaccion valida.\n";
+                esValido = false;
+            }
+            return esValido;
         }
     }
 }
