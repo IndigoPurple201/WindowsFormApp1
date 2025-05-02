2c1b9adf8798db48f890c73019e30ad03b99eb64
IndigoPurple201 <betogamboa201@hotmail.com>
2025-05-02 13:32:28 -0600

    add

--- Conectar.Designer.cs
+++ Conectar.Designer.cs
@@ -99,6 +99,7 @@
             // 
             this.txtContraseña.Location = new System.Drawing.Point(7, 33);
             this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.PasswordChar = '*';
             this.txtContraseña.Size = new System.Drawing.Size(144, 20);
             this.txtContraseña.TabIndex = 36;
             this.txtContraseña.TextChanged += new System.EventHandler(this.txtContraseña_TextChanged);
